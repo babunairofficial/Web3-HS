27c274224f4c6f03e3246ef05e09263c65c78116
Babu Nair <babunairofficial@gmail.com>
2024-10-05 20:11:29 +0530

    Web3 - assignment - prefix hash code

---
+++ Assignments/week1/assgn1.js
@@ -0,0 +1,18 @@
+// Give me an input string that outputs a SHA-256 hash that starts with 00000
+
+const crypto = require('crypto');
+
+function prefixedHash(prefix){
+    let input = 0;
+    while(true){
+        let inputStr = input.toString();
+        let hash = crypto.createHash('sha256').update(inputStr).digest('hex');
+        if(hash.startsWith(prefix)){
+            return {input:inputStr, hash:hash}
+        }
+        input++;
+    }    
+}
+const result = prefixedHash('00000');
+console.log(`Input = ${result.input}`);
+console.log(`Hash = ${result.hash}`);
