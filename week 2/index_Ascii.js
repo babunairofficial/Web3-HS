9e0f85b8af97dc11ada245fd2242e37ec907d38b
Babu Nair <babunairofficial@gmail.com>
2025-02-17 14:38:16 +0530

    ascii to bytes conversion

--- week 2/index_Ascii.js
+++ week 2/index_Ascii.js
@@ -15,3 +15,21 @@ const bytes = [66, 97, 98, 117, 32, 78, 97, 105, 114];
 const asciiString = bytesToAscii(bytes);
 //display the ascii characters
 console.log(asciiString);
+
+/**--------------------------
+ * Ascii to bytes conversion
+ * --------------------------
+ */
+//function declare
+function asciiToBytes(asciiString) {
+    //split the characters of the string
+    //map the individual characters to the function
+    //return the unicode of the character
+    return asciiString.split('').map(char => char.charCodeAt(0));
+  }
+  //ascii character
+  const ascii = "Javascript";
+  //declare a variable to store the resultant value of function called.
+  const byteArray = asciiToBytes(ascii);
+  //display the byte characters 
+  console.log(byteArray);
